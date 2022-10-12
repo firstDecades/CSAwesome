be0cf3182133329e5cfbb7ee66788de1e9977e6b
firstDecades <69013972+firstDecades@users.noreply.github.com>
2022-10-12 14:04:54 -0400

    Update Main.java

--- _sources/Unit4-Iteration/ConsumerReviewLabFiles/Main.java
+++ _sources/Unit4-Iteration/ConsumerReviewLabFiles/Main.java
@@ -1,6 +1,15 @@
 
-class Main {
-  public static void main(String[] args) {
-   
-  }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Review.sentimentVal("perfect"));
+        System.out.println(Review.sentimentVal("never"));
+        System.out.println(Review.sentimentVal("right"));
+        double num = Review.sentimentVal("warm");
+        //String word = Review.sentimentVal(0.5);
+        //double x = Review.sentimentVal("good", "bad");
+        System.out.println("Total Sentiment (26WestReview.txt): " + Review.totalSentiment("26WestReview.txt"));
+        System.out.println("Star Rating (26WestReview.txt): " + Review.starRating("26WestReview.txt"));
+        System.out.println("Total Sentiment (simpleReview.txt): " + Review.totalSentiment("simpleReview.txt"));
+        System.out.println("Star Rating (simpleReview.txt): " + Review.starRating("simpleReview.txt"));
+    }
 }
